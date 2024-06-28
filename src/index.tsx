355752644c5ce2f295ba0153cfca5a466ac10b4c
0x330a <92654767+0x330a@users.noreply.github.com>
2024-06-28 14:47:56 +1000

    fix: try content type

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ export const app = new Frog<{State: State}>({
   imageAspectRatio: "1:1",
   origin: "https://released.fyi",
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': 'image/svg+xml',
   },
   imageOptions: {
     width: 512,
